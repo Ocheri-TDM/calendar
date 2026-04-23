853b96e2b630c6b36e987322ac4ff13aeade042e
Ocheri-TDM <nikitanaumov45@gmail.com>
2026-04-23 22:05:13 +0500

    policy
    Removed the database password from settings.py for security.

--- project-time/coledge/settings.py
+++ project-time/coledge/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'schedule_db',
         'USER': 'root',
-        'PASSWORD': 'Datamir42',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
